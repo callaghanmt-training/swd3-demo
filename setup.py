873484172cefb64285a536a2f12f77cc4da9b3d5
patricia-ternes <p.ternesdallagnollo@leeds.ac.uk>
2023-11-20 14:38:04 +0000

    add installation file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="hypot",
+    version="0.1.0",
+    author="Patricia Ternes",
+    author_email="p.ternesdallagnollo@leeds.ac.uk",
+    description="The hypot SWD3 demo package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research/Learning",
+    ],
+    python_requires=">=3.9",
+)
